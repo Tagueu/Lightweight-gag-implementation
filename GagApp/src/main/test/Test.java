1045e4ad7d71b5e5bd73f3a72ac35d4db148dfcd
Joskel <joskeltagueu@gmail.com>
2021-06-08 01:33:56 +0100

    groovy is set up

--- GagApp/src/main/test/Test.java
+++ GagApp/src/main/test/Test.java
@@ -1,5 +1,23 @@
 package main.test;
+import groovy.lang.*;
 
 public class Test {
 
+	static String classPath ="E:\\PhD Recherche\\Implementation\\workspace-java\\GagApp\\bin";
+	public static void main (String[] args) {
+		
+		System.out.println("Hello world");
+		
+		Binding b =new Binding();
+		b.setVariable("obj",5);
+		GroovyShell gsh= new GroovyShell(b);
+		GroovyClassLoader cl=gsh.getClassLoader();
+		cl.addClasspath(classPath);
+		System.out.println(gsh.evaluate("main.test.Test.getFive(obj)"));
+	}
+	
+	public static int getFive(int fiveObject) {
+		
+		return fiveObject;
+	}
 }
