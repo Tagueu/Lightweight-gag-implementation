7896e43248bec3d4f6b050607e14473869309302
Joskel <joskeltagueu@gmail.com>
2021-06-08 15:44:30 +0100

    Starting playing with xtend

---
+++ GagApp/src/fr/inria/gag/specification/aspect/GAGAspect.xtend
@@ -0,0 +1,7 @@
+package fr.inria.gag.specification.aspect
+
+import fr.inria.gag.specification.GAG
+
+class GAGAspect extends GAG{
+	
+}
---
+++ GagApp/xtend-gen/fr/inria/gag/specification/aspect/GAGAspect.java
@@ -0,0 +1,7 @@
+package fr.inria.gag.specification.aspect;
+
+import fr.inria.gag.specification.GAG;
+
+@SuppressWarnings("all")
+public class GAGAspect extends GAG {
+}
